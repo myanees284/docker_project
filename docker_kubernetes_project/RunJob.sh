396398afd4f61c9e87c8a35c5bf3a0bb4433229c
myanees284 <44027805+myanees284@users.noreply.github.com>
2019-01-26 05:13:58 +0400

    Add files via upload

---
+++ docker_kubernetes_project/RunJob.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+timestamp=$(date +%s)
+imagever=myanees/aneesm:v$timestamp
+APIKEY=${clogin}
+KUBECONFIG=${kubeconfig}
+DLOGIN=${dlogin}
+DPASS=${dpass}
+docker login --username $DLOGIN --password $DPASS
+docker build -t $imagever .
+docker push $imagever
+ibmcloud login --apikey $APIKEY -r eu-de
+ibmcloud ks cluster-config mycluster
+export $KUBECONFIG
+export DEPNAME=$(kubectl get deployment -o json | jq '.items[0].metadata.name')
+echo $DEPNAME
+if [[ $DEPNAME == "\"hellosample1"\" ]]
+then
+  echo "Updating container IMAGE"
+  kubectl set image deployments/hellosample1 testla=$imagever
+else
+  echo "Creating deployment"
+  kubectl create -f Sample_Deployment.yml
+  kubectl create -f exposeservice.yml
+fi
