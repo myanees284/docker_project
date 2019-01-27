1fbbefd008aebe1ebde7f9214d7c7bb8fe4d9f61
myanees284 <44027805+myanees284@users.noreply.github.com>
2019-01-27 11:05:24 +0400

    Update RunJob.sh

--- docker_kubernetes_project/RunJob.sh
+++ docker_kubernetes_project/RunJob.sh
@@ -14,7 +14,7 @@ export $KUBECONFIG
 #export DEPNAME=$(kubectl get deployment -o json | jq '.items[0].metadata.name')
 export DEPNAME=$(kubectl get deployment --selector=$sel --output=jsonpath={.items..metadata.name})
 echo $DEPNAME
-if [[ $DEPNAME == "hellosample1" ]]
+if [[ $DEPNAME == *"hellosample1"* ]]
 then
   echo "Updating container IM66AGE"
   kubectl set image deployments/hellosample1 testla=$imagever
