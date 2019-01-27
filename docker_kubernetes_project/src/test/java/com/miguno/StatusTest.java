8a6de8257439dfe0a84e007a218ac5080c17c483
myanees284 <44027805+myanees284@users.noreply.github.com>
2019-01-27 09:38:34 +0400

    Update StatusTest.java

--- docker_kubernetes_project/src/test/java/com/miguno/StatusTest.java
+++ docker_kubernetes_project/src/test/java/com/miguno/StatusTest.java
@@ -31,6 +31,6 @@ public class StatusTest {
   @Test
   public void shouldReceiveIdleStatus() {
     String responseMsg = target.path("status").request().get(String.class);
-    assertEquals("{\"status-NEW422\": \"idle\"}\n", responseMsg);
+    assertEquals("{\"status-NEW433\": \"idle\"}\n", responseMsg);
   }
 }
