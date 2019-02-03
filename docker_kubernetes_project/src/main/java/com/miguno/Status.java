610621216e21fe0efb755d88597f9cb1b9db1355
myanees284 <44027805+myanees284@users.noreply.github.com>
2019-02-03 14:39:59 +0400

    Update Status.java

--- docker_kubernetes_project/src/main/java/com/miguno/Status.java
+++ docker_kubernetes_project/src/main/java/com/miguno/Status.java
@@ -11,6 +11,6 @@ public class Status {
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   public String statusInformation() {
-    return "{\"status-NEW444\": \"idle\"}\n";
+    return "{\"status-NEW445\": \"idle\"}\n";
   }
 }
